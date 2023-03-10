e898b11c189d6ec1e6c7513e1b8c2288eba80e5f
khangHNISC <59554204+khangHNISC@users.noreply.github.com>
2023-03-10 10:44:11 +0700

    first commit

---
+++ app/src/main/java/com/example/vvvv/HomeFragment.kt
@@ -0,0 +1,30 @@
+package com.example.vvvv
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.vvvv.databinding.FragHomeBinding
+
+class HomeFragment : Fragment() {
+    private lateinit var binding: FragHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.next.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_detailFragment)
+        }
+    }
+}
